f9401556949d8fb444a754cab63532606aaa76fe
Hannes Widmoser <widmoser@gmail.com>
2012-09-24 16:47:27 +0200

    added Printable interface

--- src/debug/print.cpp
+++ src/debug/print.cpp
@@ -4,3 +4,13 @@ std::ostream& operator<<(std::ostream& os, const std::string& s) {
 	os << s.c_str();
 	return os;
 }
+
+std::ostream& operator<<(std::ostream& os, const cpputils::debug::Printable& p) {
+	p.print(os);
+	return os;
+}
+
+std::ostream& operator<<(std::ostream& os, const cpputils::debug::Printable* p) {
+	p->print(os);
+	return os;
+}
--- src/debug/print.h
+++ src/debug/print.h
@@ -36,10 +36,20 @@ namespace cpputils {
 
 namespace debug {
 
+class Printable {
+public:
 
+	virtual ~Printable() {}
+
+	virtual void print(std::ostream& os) const = 0;
+
+};
 
 }
 
 }
+
+std::ostream& operator<<(std::ostream& os, const cpputils::debug::Printable& p);
+std::ostream& operator<<(std::ostream& os, const cpputils::debug::Printable* p);
 
 #endif /* PRINT_H_ */
